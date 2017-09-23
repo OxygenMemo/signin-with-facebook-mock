34cd8e1a0a374c2ce9f30269640f2a76e71122aa
OxygenMemo <g58160698@gmail.com>
2017-09-23 11:57:53 +0700

    psss mock facebook

--- fb.spec.js
+++ fb.spec.js
@@ -1,5 +1,30 @@
+function Auth(authService){
+    this.authService=authService
+    this.signInWithFacebook=(phone,password)=>{
+        
+        return this.authService(phone,password)
+    }
+}
 test('Mock with 1 should return 1',()=>{
-    const mock=jest.fn()
+    
+    const mockFn=jest.fn(1)
         .mockReturnValue(1)
-        expect(mock()).toBe(1)
+        expect(mockFn(1)).toBe(1)
+        expect(mockFn).toBeCalledWith(1)
+})
+test('Signin with valid credential should be pass',()=>{
+    const returnObject={
+        name:'Weera',
+        fackbookId:'1234',
+        email:'w@gmail.com'
+    }
+    const mockFacebook=jest.fn("0812345678","abc1234")
+        .mockReturnValue(returnObject)
+
+    let app =new Auth(mockFacebook)
+    let value =app.signInWithFacebook("0812345678","abc1234")
+
+    expect(value).toEqual(returnObject)//ค่าเท่า
+    expect(mockFacebook).toBeCalled()//fn ถูกเรียก
+    expect(mockFacebook).toBeCalledWith("0812345678","abc1234")//test pamamiter
 })
